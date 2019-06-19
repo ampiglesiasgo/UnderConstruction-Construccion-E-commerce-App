36248434e31e9fa54cf920bb7c0916d80c635cb8
Amparo Iglesias <ampiglesiasgo@gmail.com>
2019-06-19 19:14:06 -0300

    ml agregado

---
+++ UnderConstructionApp/ViewControllers/PhotoResultViewController.swift
@@ -0,0 +1,46 @@
+//
+//  PhotoResultViewController.swift
+//  UnderConstructionApp
+//
+//  Created by Amparo Iglesias on 6/19/19.
+//  Copyright © 2019 Amparo Iglesias. All rights reserved.
+//
+
+import UIKit
+
+class PhotoResultViewController: UIViewController {
+
+    @IBOutlet weak var photoResultaImageView: UIImageView!
+    @IBOutlet weak var classificationLabel: UILabel!
+    
+    var photoImageResult = UIImage()
+    var classifierResult = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        classificationLabel.text = classifierResult
+        photoResultaImageView.image = photoImageResult
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- UnderConstructionApp/ViewControllers/PhotoShootOrLibraryViewController.swift
+++ UnderConstructionApp/ViewControllers/PhotoShootOrLibraryViewController.swift
@@ -7,13 +7,16 @@
 //
 
 import UIKit
+import CoreML
 
 class PhotoShootOrLibraryViewController: UIViewController , UINavigationControllerDelegate {
     
     @IBOutlet weak var libraryButton: UIButton!
     @IBOutlet weak var cameraButton: UIButton!
-    var image = UIImage()
-    var classifier = String()
+    
+    var model: Inceptionv3!
+    var photoImage = UIImage()
+    var classifier = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +27,17 @@ class PhotoShootOrLibraryViewController: UIViewController , UINavigationControll
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillLayoutSubviews() {
+        libraryButton.layer.cornerRadius = 15
+        libraryButton.clipsToBounds = true
+        cameraButton.layer.cornerRadius = 15
+        cameraButton.clipsToBounds = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        model = Inceptionv3()
     }
     
     
@@ -56,5 +70,64 @@ class PhotoShootOrLibraryViewController: UIViewController , UINavigationControll
 extension PhotoShootOrLibraryViewController: UIImagePickerControllerDelegate {
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        picker.dismiss(animated: true)
+        guard let image = info["UIImagePickerControllerOriginalImage"] as? UIImage else {
+            return
+        }
+        
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: 299, height: 299), true, 2.0)
+        image.draw(in: CGRect(x: 0, y: 0, width: 299, height: 299))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var pixelBuffer : CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(newImage.size.width), Int(newImage.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+        guard (status == kCVReturnSuccess) else {
+            return
+        }
+        
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
+        
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let context = CGContext(data: pixelData, width: Int(newImage.size.width), height: Int(newImage.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
+        
+        context?.translateBy(x: 0, y: newImage.size.height)
+        context?.scaleBy(x: 1.0, y: -1.0)
+        
+        UIGraphicsPushContext(context!)
+        newImage.draw(in: CGRect(x: 0, y: 0, width: newImage.size.width, height: newImage.size.height))
+        UIGraphicsPopContext()
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        photoImage = newImage
+        
+        // Core ML
+        guard let prediction = try? model.prediction(image: pixelBuffer!) else {
+            return
+        }
+        
+        classifier = "Creo que es un \(prediction.classLabel)."
+        
+
+        self.performSegue(withIdentifier: "photoResultSegue", sender: self)
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "photoResultSegue"{
+            let photoResultViewController = (segue.destination as! PhotoResultViewController)
+            print(classifier)
+            print(photoImage)
+            photoResultViewController.classifierResult = classifier
+            photoResultViewController.photoImageResult = photoImage
+            
+        }
     }
 }
